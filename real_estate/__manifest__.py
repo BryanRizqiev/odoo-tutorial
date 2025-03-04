5026db260ca4173f9dc0740db61b2cf205dd0df8
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2025-03-04 16:12:13 +0700

    Update __manifest__.py

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -22,8 +22,9 @@
     'data': [
       'security/ir.model.access.csv',
       'views/estate_property_views.xml',
-      'views/property_offer_views.xml',
+      'views/estate_property_offer_views.xml',
       'views/estate_property_type_views.xml',
       'views/estate_property_tag_views.xml',
+      'views/res_user_view.xml',
     ]
 }
