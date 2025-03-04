9f769063c5b3bdc528c06637fb89c76d8794298d
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2025-03-04 16:11:48 +0700

    feat: update model property type

--- real_estate/models/estate_property_type.py
+++ real_estate/models/estate_property_type.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -9,8 +6,20 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char('Name', required=True)
     estate_ids = fields.One2many("estate.property", "property_type_id", string="Estate")
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
     property_ids = fields.One2many(
         'estate.property', 'property_type_id', string="Properties"
     )
+    offer_ids = fields.One2many(related="property_ids.offer_ids")
+    offer_count = fields.Integer("Offer Count", compute="_compute_offer_count", readonly=True)
+
+    _sql_constraints = [
+        ("unique_name", "unique(name)", "The type name already created"),
+    ]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.property_ids.mapped('offer_ids'))
 
 
