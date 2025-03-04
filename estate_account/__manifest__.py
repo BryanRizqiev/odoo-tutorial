114fad9fd9037c0a28c233befc0f454ee379d773
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2025-03-04 16:12:30 +0700

    fead: add estate_account module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate Account",
+    'summary': """
+        Starting module for "Discover the JS framework, chapter 1: Owl components"
+    """,
+
+    'description': """
+        Starting module for "Discover the JS framework, chapter 1: Owl components"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com",
+
+    'category': 'Tutorials/Estate Account',
+    'version': '0.1',
+
+    'depends': ['base', 'real_estate', 'account'],
+    'application': True,
+    'installable': True,
+    'license': 'AGPL-3',
+    'data': []
+}
