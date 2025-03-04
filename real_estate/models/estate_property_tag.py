7ca174c54fa407589d86a40d492ed9fc59d6a206
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2025-03-04 16:11:44 +0700

    feat: update model property tag

--- real_estate/models/estate_property_tag.py
+++ real_estate/models/estate_property_tag.py
@@ -1,10 +1,19 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+from random import randint
 from odoo import fields, models
 
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate Property Tag model"
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char("Name", required=True)
+    property_id = fields.Many2one("estate.property", string="Property", ondelete="cascade")
+    color = fields.Integer(
+        string="Color Index", default=lambda self: self._default_color(),
+        help="Tag color.")
+
+    _sql_constraints = [
+        ("unique_name", "unique(name)", "The tag name already created"),
+    ]
+
+    def _default_color(self):
+        return randint(1, 11)
